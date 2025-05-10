de499f67feba4643b8dd4001326b6a97d7404995
高森暖大 <haruto@takamoridandainoMacBook-Air.local>
2025-05-10 21:23:28 +0900

    キャッシュ戦略の修正

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,55 +1,62 @@
 // キャッシュの名前とバージョン
-const CACHE_NAME = 'budget-app-v1';
+const CACHE_NAME = "budget-app-v1";
 
 // キャッシュするファイル
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/main.chunk.js',
-  '/static/js/bundle.js',
-  '/manifest.json',
-  '/favicon.ico',
-  '/Icon-192.png',
-  '/Icon-512.png',
+  "/",
+  "/index.html",
+  "/static/js/main.chunk.js",
+  "/static/js/bundle.js",
+  "/manifest.json",
+  "/favicon.ico",
+  "/Icon-192.png",
+  "/Icon-512.png",
 ];
 
 // インストール時のキャッシュ
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // キャッシュの使用とフォールバック
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
+self.addEventListener("fetch", (event) => {
+  if (
+    event.request.url.includes("Icon-192.png") ||
+    event.request.url.includes("Icon-512.png")
+  ) {
+    event.respondWith(caches.match(event.request)); // キャッシュから取得
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
         // キャッシュが見つかればそれを返す
         if (response) {
           return response;
         }
-        
+
         // キャッシュにない場合はネットワークリクエスト
-        return fetch(event.request)
-          .then(response => {
-            // レスポンスが有効かを確認
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // レスポンスをクローンしてキャッシュに追加
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-              
+        return fetch(event.request).then((response) => {
+          // レスポンスが有効かを確認
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
             return response;
+          }
+
+          // レスポンスをクローンしてキャッシュに追加
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
           });
+
+          return response;
+        });
       })
-  );
+    );
+  }
 });
