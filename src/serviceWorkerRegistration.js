596347301557fca8798ec0134676cb1bf4a8c53d
高森暖大 <haruto@takamoridandainoMacBook-Air.local>
2025-05-10 21:49:08 +0900

    キャッシュ戦略の修正

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,15 +1,16 @@
 // Service Workerを登録する関数
 export function register() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      
-      navigator.serviceWorker.register(swUrl)
-        .then(registration => {
-          console.log('Service Worker登録成功:', registration);
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      const swUrl = `${window.location.origin}/service-worker.js`;
+
+      navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+          console.log("Service Worker登録成功:", registration);
         })
-        .catch(error => {
-          console.error('Service Worker登録失敗:', error);
+        .catch((error) => {
+          console.error("Service Worker登録失敗:", error);
         });
     });
   }
@@ -17,12 +18,12 @@ export function register() {
 
 // Service Workerを解除する関数
 export function unregister() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready
-      .then(registration => {
+      .then((registration) => {
         registration.unregister();
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error.message);
       });
   }
